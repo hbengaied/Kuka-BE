812027da9bd702abcbf242616d3d1a8bbb965393
hicheme ben-gaied <hicheme20@hotmail.fr>
2023-03-28 15:05:01 +0100

    peut ecrire des phrases ok

--- ConnectionManager.py
+++ ConnectionManager.py
@@ -11,7 +11,7 @@ class ReceiveThread(threading.Thread): # cette classe servira principalement à 
         self.ip = ipclient
         self.port = portClient
         self.clientsocket = clientsocket
-        MM.Phrase.Initialisation("ambmcmdmemfm")
+        MM.Phrase.Initialisation("salut yousri le bg")
 
 
     def run(self): # cette fonction est appelé lorsque kuka a réussi à se connecter
@@ -27,20 +27,28 @@ class ReceiveThread(threading.Thread): # cette classe servira principalement à 
             f.write(bytes.decode(self.data_received))
 
             f.close() # je m'assure que le fichier est bien fermé et donc que l'écriture est terminé avant de commencer à répondre au robot
+            if MM.Phrase.Check == False :
+                Conversation.Response(MM.Phrase.MyText[MM.Phrase.Compteur]) # c'est ici qu'il commence l'échange avec le kuka
+                MM.Phrase.Check = Verification.Verif(MM.Phrase.MyText[MM.Phrase.Compteur])
 
-            Conversation.Response(MM.Phrase.MyText[MM.Phrase.Compteur]) # c'est ici qu'il commence l'échange avec le kuka
-
-            check = Verification.Verif(MM.Phrase.MyText[MM.Phrase.Compteur])
-            if check == True :
-                print("++++++++++++++++++++++++++++++++Je suis dans arrivé ou je voulais++++++++++++++++++++++++++++++++++++++++")
+            if MM.Phrase.Check == True :
+                print("++++++++++++++++++++++++++++++++OUAIS MA CITEEEEEEEEEEEEEEEEEEEEE++++++++++++++++++++++++++++++++++++++++")
                 print("Jai pu cliquer sur la touche :", MM.Phrase.MyText[MM.Phrase.Compteur])
 
+                #Ici lorsque la lettre voulu a été atteinte, on va relever l'outil de +150 en Z
+                if MM.Phrase.CheckUp == False :
+                    ConversationGoUp.ResponseGoUp(MM.Phrase.MyText[MM.Phrase.Compteur])
+                    MM.Phrase.CheckUp = GoUp.VerifGoUp(MM.Phrase.MyText[MM.Phrase.Compteur])
                 #Je vais passer caractere suivant
-                MM.Phrase.Compteur = MM.Phrase.Compteur +1
-                if MM.Phrase.Compteur == MM.Phrase.TaillePhrase:
-                    print("On recommence le mot pcq on vient d'arriver à la fin pour eviter le bug ! Merci de mettre une version plus propre pour gerer ce cas")
-                    MM.Phrase.Compteur = 0
-                check = False
+                if MM.Phrase.CheckUp == True and MM.Phrase.Check == True :
+                    MM.Phrase.Compteur = MM.Phrase.Compteur +1
+                    #Ca c'est juste pour eviter un depassement index quand on arrive fin du mot ! Faut impltementer un autre truc plus propre et plus pro 
+                    if MM.Phrase.Compteur == MM.Phrase.TaillePhrase:
+                        print("On recommence le mot pcq on vient d'arriver à la fin pour eviter le bug ! Merci de mettre une version plus propre pour gerer ce cas")
+                        MM.Phrase.Compteur = 0
+                    MM.Phrase.Check = False
+                    MM.Phrase.CheckUp = False
+
 
 
 
@@ -109,6 +117,69 @@ class Verification():
             PosX = float(MM.Dictionnarie.Dico[lettre]["X"])
             PosY = float(MM.Dictionnarie.Dico[lettre]["Y"])
             PosZ = float(MM.Dictionnarie.Dico[lettre]["Z"])
+            PosA = float(MM.Dictionnarie.Dico[lettre]["A"])
+            PosB = float(MM.Dictionnarie.Dico[lettre]["B"])
+            PosC = float(MM.Dictionnarie.Dico[lettre]["C"])
+            if abs(PosX-x) <0.3 and abs(PosY-y) <0.3 and abs(PosZ-z)<0.3 :
+                return True
+        except:
+            pass
+        return False
+
+
+class ConversationGoUp():
+    @staticmethod
+    def ResponseGoUp(lettre):
+        try:
+            tree = ET.parse("input.xml") # je parse le fichier xml
+            root = tree.getroot() # je prend la balise root du fichier
+
+            ipoc_balise = "" # cette variable va contenir le timestamps que je devrais renvoyé au kuka
+            for element in root: #pour chaque élement de la balise root, si le tag est IPOC alors je prend ce que contient la balise IPOC
+                if element.tag == "IPOC":
+                    ipoc_balise = element
+            PosZ = float(MM.Dictionnarie.Dico[lettre]["Z"]) + 75
+            StringPosZ = str(PosZ)
+            Data = { # j'initialise le dictionnaire de donnée à envoyé au robot, ATTENTION 
+                "X" : MM.Dictionnarie.Dico[lettre]["X"],
+                "Y" : MM.Dictionnarie.Dico[lettre]["Y"],
+                "Z" : StringPosZ,
+                "A" : MM.Dictionnarie.Dico[lettre]["A"],
+                "B" : MM.Dictionnarie.Dico[lettre]["B"],
+                "C" : MM.Dictionnarie.Dico[lettre]["C"],
+                "IPOC" : ipoc_balise.text
+            }
+
+            XmlManager.XmlManager.SetDataToSend(Data) # cette fonction sert à initialisé le fichier xml "output.xml"
+
+            SendData.Send() # et juste aprés, il envoie le fichier xml
+            
+        except:
+            pass
+
+
+class GoUp():
+    @staticmethod
+    def VerifGoUp(lettre):
+        try:
+            # parse the XML file
+            tree = ET.parse('input.xml')
+
+            # get the root element of the XML file
+            root = tree.getroot()
+
+            # get the values of X, Y, Z, A, B, C from the RIst element
+            rist = root.find('RIst')
+            x = float(rist.get('X'))
+            y = float(rist.get('Y'))
+            z = float(rist.get('Z'))
+            a = float(rist.get('A'))
+            b = float(rist.get('B'))
+            c = float(rist.get('C'))
+
+            PosX = float(MM.Dictionnarie.Dico[lettre]["X"])
+            PosY = float(MM.Dictionnarie.Dico[lettre]["Y"])
+            PosZ = float(MM.Dictionnarie.Dico[lettre]["Z"]) + 75
             PosA = float(MM.Dictionnarie.Dico[lettre]["A"])
             PosB = float(MM.Dictionnarie.Dico[lettre]["B"])
             PosC = float(MM.Dictionnarie.Dico[lettre]["C"])
