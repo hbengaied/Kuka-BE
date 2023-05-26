05533eb60beb42069b2de9305f4b92e802888e03
hicheme ben-gaied <hicheme20@hotmail.fr>
2023-05-27 00:25:29 +0100

    Fichier MouvementZ commenté

--- Src/MouvementZ.py
+++ Src/MouvementZ.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import XmlManager
 
 class MoveMyZ():
+    #Methode qui permet de faire descendre le bras sur la touche du clavier souhaité passé en arguement afin de procéder
+    #au clic de la touche
+    #Cette dernière prend une "lettre" qui contient les coords de la touche
     @staticmethod
     def MoveZDown(lettre):
         try:
@@ -29,7 +32,9 @@ class MoveMyZ():
             
         except:
             pass
-
+    
+    #Methode qui permet de faire monter le bras au dessus de la touche du clavier cliqué pour faire le prochain déplacement
+    #Cette dernière prend une "lettre" qui contient les coords de la touche, et on ajoute +35 au Z qui est la marge de sécurité
     @staticmethod
     def MoveZUp(lettre):
         try:
